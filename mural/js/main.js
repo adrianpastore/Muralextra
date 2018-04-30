8191c198629c513f8a3eb775d018e52210352a04
Adrian Pastore <adrianqpastore@gmail.com>
2018-04-29 23:45:16 -0300

    Update main.js

--- mural/js/main.js
+++ mural/js/main.js
@@ -5,17 +5,21 @@ texto = document.getElementById('textarea');
 
 
 linha = document.getElementById('linha');
+linha2 = document.getElementById('linha2')
 botao.addEventListener('click', function (evento) {
-  tamanhomaximo = linha.childNodes.length;
-    if(tamanhomaximo<=4){
-      let elemento = document.createElement('td');
-      elemento.setAttribute("class","td");
-      let check = document.createElement('input');
-      check.setAttribute("type", "checkbox");
-      elemento.textContent = texto.value;
-      elemento.appendChild(check);
-      linha.appendChild(elemento);
-      mural.appendChild(linha);
+  let elemento = document.createElement('td');
+  elemento.setAttribute("class","td");
+  let check = document.createElement('input');
+  check.setAttribute("type", "checkbox");
+  elemento.textContent = texto.value;
+  elemento.appendChild(check);
+  if(linha.childNodes.length<4){
+    linha.appendChild(elemento);
+    mural.appendChild(linha);
+  }
+  if(linha2.childNodes.length<4){
+    linha2.appendChild(elemento);
+    mural.appendChild(linha2);
     }
 });
 
@@ -31,3 +35,16 @@ excluir.addEventListener('click', function (evento) {
     }
   }
 });
+
+excluir.addEventListener('click', function (evento) {
+    let tamanho = linha2.childNodes.length;
+    var i=0;
+    while(i<tamanho){
+      if (linha2.children[i].children[0].checked == true) {
+        linha2.childNodes[i].parentNode.removeChild(linha2.childNodes[i]);
+        i=0;
+      }else{
+        i++;
+      }
+    }
+  });
